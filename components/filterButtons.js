e3e2173ad43bf1212acda63d32cb37807d4a6e77
Steve Butler <butlersteve.c@gmail.com>
2023-03-05 18:54:29 -0600

    add filterEvents.js and filterByLangTech to vocabRequests.js

--- components/filterButtons.js
+++ components/filterButtons.js
@@ -2,10 +2,10 @@ import renderToDom from '../utils/renderToDom';
 
 const filterButtons = () => {
   const domString = `
-  <button type="button" class="btn btn-dark btn-sm">HTML</button>
-  <button type="button" class="btn btn-dark btn-sm">CSS</button>
-  <button type="button" class="btn btn-dark btn-sm">JavaScript</button>
-  <button type="button" class="btn btn-dark btn-sm">REACT</button>
+  <button type="button" id="htmlFilter" class="btn btn-dark btn-sm">HTML</button>
+  <button type="button" id="cssFilter" class="btn btn-dark btn-sm">CSS</button>
+  <button type="button" id="javascriptFilter" class="btn btn-dark btn-sm">JavaScript</button>
+  <button type="button" id="reactFilter" class="btn btn-dark btn-sm">REACT</button>
   `;
 
   renderToDom('#filterButtonContainer', domString);
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,14 +1,16 @@
 import logoutButton from '../components/logoutButton';
-import getVocabulary from '../components/vocabRequests';
+import { getVocabulary } from '../api/vocabRequests';
 import buildDom from '../pages/buildDom';
 import showVocab from '../pages/showVocab';
 import navBar from '../components/navBar';
 import filterButtons from '../components/filterButtons';
+import filterEvents from '../events/filterEvents';
 
 const startApp = () => {
   buildDom();
   navBar();
   filterButtons();
+  filterEvents();
   logoutButton();
 
   getVocabulary().then((data) => {
